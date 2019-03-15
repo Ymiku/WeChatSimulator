4a8f672ff07278d7af9f74ec292fa13c7a7066cd
Frost <ymiku@qq.com>
2019-03-15 11:15:38 +0800

    Create EmptyGraphic.cs

---
+++ Assets/Scripts/UIFramework/Foundation/UIComponent/EmptyGraphic.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace UnityEngine.UI
+{
+	public class EmptyGraphic : MaskableGraphic {
+
+		protected EmptyGraphic()
+		{
+			useLegacyMeshGeneration = false;
+		}
+		protected override void OnPopulateMesh (VertexHelper vh)
+		{
+			vh.Clear ();
+		}
+	}
+}
