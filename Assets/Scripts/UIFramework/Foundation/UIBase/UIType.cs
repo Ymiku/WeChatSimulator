85cb40f5f39781430c269efc29c0a5000352cb55
Rhenger <59799503@qq.com>
2019-03-19 19:10:17 +0800

    rh:提交ui

--- Assets/Scripts/UIFramework/Foundation/UIBase/UIType.cs
+++ Assets/Scripts/UIFramework/Foundation/UIBase/UIType.cs
@@ -45,5 +45,6 @@ namespace UIFrameWork
 		public static readonly UIType TotalAssets = new UIType("View/TotalAssetsView");
 		public static readonly UIType TransDetails = new UIType("View/TransDetailsView"); //转账明细界面
 		public static readonly UIType YuEBao = new UIType("View/YuEBaoView"); //余额宝界面
+		public static readonly UIType AntCredit = new UIType("View/AntCreditView"); //花呗界面
     }
 }
