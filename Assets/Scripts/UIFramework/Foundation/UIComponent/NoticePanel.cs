b882853467d1047c88d547dd67767d94dbd8491f
Haoran Liu <ymiku@qq.com>
2019-03-27 00:21:10 +0800

    notice

---
+++ Assets/Scripts/UIFramework/Foundation/UIComponent/NoticePanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NoticePanel : MonoBehaviour {
+    public Text text;
+    public CanvasGroup group;
+    Stack<string> _noticeStack = new Stack<string>();
+ 
+    public void AddNotice(string notice)
+    {
+        _noticeStack.Push(notice);
+    }
+    public void Close()
+    {
+        group.alpha = 0.0f;
+        group.blocksRaycasts = false;
+    }
+
+}
