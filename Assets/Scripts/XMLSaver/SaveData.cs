0b2f2fb8a6296b0b80364c6f92bf11070fae6535
Frost <ymiku@qq.com>
2019-03-27 19:46:33 +0800

    Update SaveData.cs

--- Assets/Scripts/XMLSaver/SaveData.cs
+++ Assets/Scripts/XMLSaver/SaveData.cs
@@ -5,19 +5,6 @@ using UnityEngine;
 [System.Serializable]
 public partial class SaveData
 {
-    public List<string> names = new List<string>();
-    //0是主角
-    public List<CharacterData> charDatas = new List<CharacterData>();
+    
 
-}
-[System.Serializable]
-public class CharacterData
-{
-    public List<CardData> cardDatas = new List<CardData>();
-}
-[System.Serializable]
-public class CardData
-{
-    public int cardId;
-    public float balance;
 }
