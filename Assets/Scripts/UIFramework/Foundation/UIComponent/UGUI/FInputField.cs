bbefbbb4babd14908cf6e8d21a1593578f38adbb
Frost <ymiku@qq.com>
2019-03-29 13:55:12 +0800

    Update FInputField.cs

--- Assets/Scripts/UIFramework/Foundation/UIComponent/UGUI/FInputField.cs
+++ Assets/Scripts/UIFramework/Foundation/UIComponent/UGUI/FInputField.cs
@@ -26,31 +26,31 @@ namespace UnityEngine.UI
         string line = ".";
         protected void FOnValueChange(string s)
         {
-            content = Utils.FormatStringForInputField(s,FInputType.PhoneNumber);
-            UpdateFocusEffect();
+			s = s.Replace (" ","");
+			text = Utils.FormatStringForInputField(s,FInputType.PhoneNumber);
+			//if (text [caretPosition - 1].Equals (" "))
+			//	caretPosition = selectionFocusPosition = caretPositionInternal = caretSelectPositionInternal = caretPosition + 1;
+			//UpdateLabel ();
         }
-        int focusChangeRXId;
-        int lineChangeRXId;
         protected override void OnEnable()
         {
             base.OnEnable();
             onValueChanged.AddListener(FOnValueChange);
-            focusChangeRXId = FrostRX.Start(this).ExecuteWhen(() => { UpdateFocusEffect(); }, () => { return isFocused; }).
-                ExecuteWhen(() => { UpdateFocusEffect(); }, () => { return !isFocused; }).GoToBegin().GetId();
-            lineChangeRXId = FrostRX.Start(this).ExecuteAfterTime(() => { line = ""; if(isFocused)UpdateFocusEffect(); }, 1.0f).
-                ExecuteAfterTime(() => { line = "."; if (isFocused) UpdateFocusEffect(); }, 1.0f).GoToBegin().GetId();
-
+		
         }
         protected override void OnDisable()
         {
             base.OnDisable();
             onValueChanged.RemoveListener(FOnValueChange);
-            FrostRX.Instance.EndRxById(focusChangeRXId);
-            FrostRX.Instance.EndRxById(lineChangeRXId);
         }
-        private void UpdateFocusEffect()
-        {
-            f_TextComponent.text = content+(isFocused?line:"");
-        }
+		void Update()
+		{
+			if (caretPosition - 1 < 0)
+				return;
+			
+			if (text [caretPosition - 1].Equals(' ')) {
+				caretPosition ++;
+			}
+		}
     }
 }
