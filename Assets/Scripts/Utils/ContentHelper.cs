fa91921020d50a589d66f70e7270857889fc9a61
Rhenger <59799503@qq.com>
2019-03-29 13:38:08 +0800

    rh:提交转账

--- Assets/Scripts/Utils/ContentHelper.cs
+++ Assets/Scripts/Utils/ContentHelper.cs
@@ -8,4 +8,5 @@ public class ContentHelper{
     //content表里id的define
     public const int CanNotTransToSelf = 2;  //不能给自己转账  
     public const int TransAccountNotExist = 3;  //账户不存在或对方设置了隐私保护
+    public const int IllegalInput = 4;  //您的输入不合法
 }
