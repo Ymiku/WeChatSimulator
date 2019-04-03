31ca107174fb5dd3a5be37c4cd8a1f2a219efd3a
Frost <ymiku@qq.com>
2019-04-03 17:10:43 +0800

    chat

---
+++ Assets/Scripts/UIFramework/ItemScript/ChatItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatItem : ItemBase {
+	public ImageProxy head;
+	public TextProxy name;
+	public TextProxy message;
+	public GameObject redPoint;
+	public TextProxy redCount;
+	public override void SetData (object o)
+	{
+		base.SetData (o);
+	}
+}
