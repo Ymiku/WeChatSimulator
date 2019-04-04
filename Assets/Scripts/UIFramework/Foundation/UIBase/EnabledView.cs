e7489e30c8190b3372d945c9a7f4191cecdfba14
Frost <ymiku@qq.com>
2019-04-04 12:32:59 +0800

    Update EnabledView.cs

--- Assets/Scripts/UIFramework/Foundation/UIBase/EnabledView.cs
+++ Assets/Scripts/UIFramework/Foundation/UIBase/EnabledView.cs
@@ -7,6 +7,8 @@ namespace UIFrameWork
 {
 	public abstract class EnabledView : BaseView 
 	{
+		float disappearTime = 4.0f;
+		int rxId = -1;
 		public override void OnEnter(BaseContext context)
 		{
 			base.OnEnter (context);
@@ -16,19 +18,25 @@ namespace UIFrameWork
 		public override void OnExit(BaseContext context)
 		{
 			base.OnExit (context);
-			gameObject.SetActive (false);
+			FrostRX.Instance.EndRxById (rxId);
+			rxId = FrostRX.Start (this).ExecuteAfterTime(()=>{gameObject.SetActive (false);rxId=-1;},disappearTime).GetId();
 		}
 		public override void OnResume (BaseContext context)
 		{
 			base.OnResume (context);
+			FrostRX.Instance.EndRxById (rxId);
 			if(!activeWhenPause)
 				gameObject.SetActive (true);
 		}
 		public override void OnPause (BaseContext context)
 		{
 			base.OnPause (context);
-			if(!activeWhenPause)
-				gameObject.SetActive (false);
+			if (!activeWhenPause) {
+				FrostRX.Instance.EndRxById (rxId);
+				rxId = FrostRX.Start (this).ExecuteAfterTime (() => {
+					gameObject.SetActive (false);rxId = -1;
+				}, disappearTime).GetId ();
+			}
 		}
 	}
 }
