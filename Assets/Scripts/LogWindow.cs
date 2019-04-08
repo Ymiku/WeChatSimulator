daf1967e9a55ca2b1b267dd5e41f5203d4082ec2
Frost <ymiku@qq.com>
2019-04-08 10:23:13 +0800

    log

---
+++ Assets/Scripts/LogWindow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.UI;
+public class LogWindow : MonoBehaviour {
+	// Use this for initialization
+	StringBuilder sb = new StringBuilder();
+	public GameObject logWin;
+	public Text log;
+	void Awake() {
+		sb.Length = 0;
+		Application.logMessageReceived += LogCallback;
+		Debug.Log ("sadsd");
+		Debug.Log ("sadsd");
+		Debug.Log ("sadsd");
+		Debug.Log ("sadsd");
+	}
+	public void LogCallback (string condition, string stackTrace, LogType type)
+	{
+		sb.Append(condition+"\n");
+		log.text = sb.ToString ();
+	}
+	public void OnClick()
+	{
+		logWin.SetActive (!logWin.activeSelf);
+	}
+}
