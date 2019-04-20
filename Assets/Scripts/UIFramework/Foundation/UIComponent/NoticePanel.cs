b839eabfb71e7cfe1a2872caa0f9088f9869f0ec
Haoran Liu <ymiku@qq.com>
2019-04-21 00:44:27 +0800

    notice

--- Assets/Scripts/UIFramework/Foundation/UIComponent/NoticePanel.cs
+++ Assets/Scripts/UIFramework/Foundation/UIComponent/NoticePanel.cs
@@ -16,7 +16,7 @@ public class NoticePanel : MonoBehaviour {
         FrostRX.Instance.StartRX().ExecuteWhen(() => { text.text = _noticeQueue.Dequeue(); group.blocksRaycasts = true; },
             () => { return _noticeQueue.Count != 0; }).
             ExecuteUntil(() => { group.alpha = Mathf.Lerp(group.alpha, 1.2f, 4.0f * Time.deltaTime); }, () => { return group.alpha >= 1.0f; }).
-            Wait(1.0f).
+            Wait(2.0f).
             ExecuteUntil(() => { group.alpha = Mathf.Lerp(group.alpha, -0.2f, 4.0f * Time.deltaTime); }, () => { return group.alpha <= 0.0f; }).
             Execute(() => { group.blocksRaycasts = false; }).
             GoToBegin().GetId(rxId);
