e4b100861abdaa7c89c4bb3ada44935c8e16d913
Frost <ymiku@qq.com>
2019-05-06 12:02:21 +0800

    scene

---
+++ Assets/Scripts/Preload.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Preload : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		SceneManager.LoadSceneAsync ("Main",LoadSceneMode.Additive);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
